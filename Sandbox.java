f8008f060decebdeddb62135678e5fd88b857d28
Nate Morrical <natemorrical@gmail.com>
2023-05-07 15:41:14 -0600

    added another line to print

--- Sandbox.java
+++ Sandbox.java
@@ -4,5 +4,6 @@ public class Sandbox {
     public static void main(String[] args) {
         // Write your program here
         System.out.println("siema");
+        System.out.println("Another line to print");
     }
 }
